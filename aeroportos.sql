5e247c64485e8b1a3c31f1e25a6d8be5fcaa3562
Pedro Felipe <pedro@pedrofelipe.com.br>
2011-08-28 17:43:12 -0300

    1.1.2 - Lista de aeroportos aprimorada
    - Lista de aeroportos aprimorada, códigos inexistentes e campos vazios
    foram removidos.

--- aeroportos.sql
+++ aeroportos.sql
@@ -1,39 +1,16 @@
--- phpMyAdmin SQL Dump
--- version 3.4.3.2
--- http://www.phpmyadmin.net
---
--- Servidor: localhost
--- Tempo de Geração: 28/08/2011 às 17h25min
--- Versão do Servidor: 5.1.56
--- Versão do PHP: 5.2.6
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Banco de Dados: `aero_aeroportos`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `aeroportos`
---
 
 CREATE TABLE IF NOT EXISTS `aeroportos` (
   `IATA` varchar(3) DEFAULT NULL,
   `ICAO` varchar(4) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `aeroportos`
---
 
 INSERT INTO `aeroportos` (`IATA`, `ICAO`) VALUES
 ('GKA', 'AYGA'),
